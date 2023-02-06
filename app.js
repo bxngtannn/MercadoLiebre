2d59984d0c8604008d88bb183d7c11c99db7e9d8
bxngtannn <dxnt3dream@gmail.com>
2023-02-06 12:06:59 -0300

    Clases anteriores hasta 0602

--- app.js
+++ app.js
@@ -10,4 +10,13 @@ app.get("/",(req,res)=>{
     res.sendFile(path.resolve('./views/home.html'));
 })
 
+app.get("/register",(req,res)=>{
+    res.sendFile(path.resolve('./views/register.html'));
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(path.resolve('./views/login.html'));
+})
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
